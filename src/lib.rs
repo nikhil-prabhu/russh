a1f9498a7457eed6b9366ff286df3fe93e5efbe7
Nikhil Prabhu <nikhilprabhu98@gmail.com>
2023-10-07 13:27:48 +0530

    feat: Initialize pymodule

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,6 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
-}
+use pyo3::prelude::*;
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
+#[pymodule]
+fn russh(_py: Python<'_>, _m: &PyModule) -> PyResult<()> {
+    Ok(())
 }
